41c40459cd1175116f63632f04b77dd86ded690f
Ryan Jones <ryanj@rmilaser.net>
2015-12-04 15:06:26 -0700

    Implimented finding ports through signal iteration

--- src/tgt-aig/aig-target.c
+++ src/tgt-aig/aig-target.c
@@ -4,13 +4,19 @@
 # include <string.h>
 
 // Logging
-#define DEBUG0(...)  printf("\e[1;32m AIG TARGET DEBUG:  \e[0m \t" __VA_ARGS__)
-#define WARNING(...) printf("\e[0;33m AIG TARGET WARNING:\e[0m \t" __VA_ARGS__)
-#define INFO(...)    printf("\e[1;34m AIG TARGET INFO:   \e[0m \t" __VA_ARGS__)
+#define LOG_LEVEL_DEBUG0
+#ifdef  LOG_LEVEL_DEBUG0
+#define DEBUG0(...)  printf("\e[1;32m AIG TARGET DEBUG   \e[0m| \t" __VA_ARGS__)
+#else
+#define DEBUG0(...)
+#endif
+#define WARNING(...) printf("\e[1;33m AIG TARGET WARNING \e[0m| \t" __VA_ARGS__)
+#define ERROR(...)   printf("\e[1;31m AIG TARGET ERROR   \e[0m| \t" __VA_ARGS__)
+#define INFO(...)    printf("\e[1;34m AIG TARGET INFO    \e[0m| \t" __VA_ARGS__)
 
 
 // Method Declarations
-int process_scope(ivl_scope_t * scope);
+int process_scope(ivl_scope_t scope);
 static int show_process(ivl_process_t net, void * x);
 int show_constants(ivl_design_t des);
 static int process_statements(ivl_statement_t net, int level);
@@ -38,14 +44,14 @@ int target_design(ivl_design_t des)
   int idx = 0;
   DEBUG0("Processing %d scopes\n",num_scopes);
   while (idx < num_scopes){
-    process_scope(*scopes++);
+    process_scope(**scopes++);
     idx++;
   }
 
   // Parse Design Processes
   ivl_design_process(des,show_process,0);
 
-  //show_constants(des);
+  show_constants(des);
 
   //Close File
 
@@ -55,40 +61,36 @@ int target_design(ivl_design_t des)
   return 0;
 }
 
-int process_scope(ivl_scope_t * scope){
-  DEBUG0("Scope name: %s\n",ivl_scope_name(*scope));
+int process_scope(ivl_scope_t scope){
+  DEBUG0("Scope name: %s\n",ivl_scope_name(scope));
+
+  //Iterate through signals to find output ports
+  DEBUG0("Determining output pins within scope\n");
+  int idx;
+  for ( idx = 0; idx < ivl_scope_sigs(scope); idx++ ) {
+    ivl_signal_t sig = ivl_scope_sig(scope,idx);
+    switch(ivl_signal_port(sig)){
+      case IVL_SIP_NONE:
+        DEBUG0("Not an port signal (%s)\n",ivl_signal_basename(sig));
+        break;
+      case IVL_SIP_INOUT:
+        WARNING("Inout signal not supported(%s)\n",ivl_signal_basename(sig));
+        INFO("Treating signal as input\n");
+      case IVL_SIP_INPUT:
+        DEBUG0("Input signal (%s)\n",ivl_signal_basename(sig));
+        break;
+      case IVL_SIP_OUTPUT:
+        INFO("Output signal (%s)\n",ivl_signal_basename(sig));
+        break;
+
+      default:
+        ERROR("Unknown signal type\n");
+        return -1;
+    }
+  }
   return 0;
 }
-/*
-      IVL_ST_NONE    = 0,
-      IVL_ST_NOOP    = 1,
-      IVL_ST_ALLOC   = 25,
-      IVL_ST_ASSIGN    = 2,
-      IVL_ST_ASSIGN_NB = 3,
-      IVL_ST_BLOCK   = 4,
-      IVL_ST_CASE    = 5,
-      IVL_ST_CASER   = 24, // Case statement with real expressions.
-      IVL_ST_CASEX   = 6,
-      IVL_ST_CASEZ   = 7,
-      IVL_ST_CASSIGN = 8,
-      IVL_ST_CONDIT  = 9,
-      IVL_ST_CONTRIB = 27,
-      IVL_ST_DEASSIGN = 10,
-      IVL_ST_DELAY   = 11,
-      IVL_ST_DELAYX  = 12,
-      IVL_ST_DISABLE = 13,
-      IVL_ST_FORCE   = 14,
-      IVL_ST_FOREVER = 15,
-      IVL_ST_FORK    = 16,
-      IVL_ST_FREE    = 26,
-      IVL_ST_RELEASE = 17,
-      IVL_ST_REPEAT  = 18,
-      IVL_ST_STASK   = 19,
-      IVL_ST_TRIGGER = 20,
-      IVL_ST_UTASK   = 21,
-      IVL_ST_WAIT    = 22,
-      IVL_ST_WHILE   = 23
-*/
+
 static int show_process(ivl_process_t net, void * x){
 
   process_statements(ivl_process_stmt(net),0);
@@ -105,9 +107,10 @@ static int process_statements(ivl_statement_t net,int level){
       if ( level != 0 ){
         WARNING("Blocking statements not supported\tLine: %d\tFile: %s\n",ivl_stmt_lineno(net),ivl_stmt_file(net));
         INFO("*** Treating blocking assignment as non-blocking assignment ***\n");
-      }else
+      }else{
         WARNING("Assign Initial Condition statement not supported\tLine: %d\tFile: %s\n",ivl_stmt_lineno(net),ivl_stmt_file(net));
-      break;
+        break;
+      }
     case IVL_ST_ASSIGN_NB:
       DEBUG0("Non-blocking assign statement\n");
       break;
@@ -128,13 +131,15 @@ static int process_statements(ivl_statement_t net,int level){
 }
 
 int show_constants(ivl_design_t des){
+  /*
   unsigned i;
 
   for(i=0;i<ivl_design_consts(des);i++){
-    ivl_net_const_t net_const = ivl_design_const(des,i);
-    //ivl_variable_type_t var_type = ivl_const_type(net_const);
+    ivl_net_const_t net= ivl_design_const(des,i);
+    //ivl_variable_type_t var_type = ivl_const_type(net);
     DEBUG0("%d\tDesign Constant type \n",i);
   }
+  */
   return 0;
 }
 
